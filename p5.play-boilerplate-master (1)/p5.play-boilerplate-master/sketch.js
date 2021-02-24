7bf9c98c22d0d910a782ba9432e7a1ecf012f27f
Smaran24Pokala <78086504+Smaran24Pokala@users.noreply.github.com>
2021-02-24 13:53:23 +0530

    Add files via upload

---
+++ p5.play-boilerplate-master (1)/p5.play-boilerplate-master/sketch.js
@@ -0,0 +1,60 @@
+//1 declare physics engine
+const Engine = Matter.Engine
+const World = Matter.World
+const Bodies =Matter.Bodies
+
+//2 local objects for engine and world
+var engine,world,ground,ball;
+
+function setup() {
+  createCanvas(800,800);
+  createSprite(400, 200, 50, 50);
+
+  //3 joining local object from the original one
+  engine=Engine.create();
+  world=engine.world;
+
+
+  //4 create the component of ground
+  //4.1 create the physics of ground
+  var ground_options={
+    isStatic : true
+  }
+
+  //4.2 create ground
+  ground = Bodies.rectangle(400,400,800,10,ground_options)
+
+  //4.3 add the component in the world
+  World.add(world,ground);
+
+
+  //ball
+  var ball_options = {
+    restitution : 1.0
+  }
+
+  ball=Bodies.circle(200,200,40,ball_options);
+
+
+  World.add(world,ball);
+
+
+}
+
+function draw() {
+  background("cyan"); 
+  
+  //5 updating the Engine(main engine) with engine(local engine)
+  Engine.update(engine);
+
+
+
+  //6 display of component
+  rectMode(CENTER)
+  rect(ground.position.x,ground.position.y,800,10);
+
+
+  ellipseMode(RADIUS)
+  ellipse(ball.position.x,ball.position.y,40,40);
+  drawSprites();
+}
